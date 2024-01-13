6d36cb7bd72d403a83864cf3c2f50088904a04b5
KutayGuler <s7019@horyzont.eu>
2024-01-14 00:15:40 +0100

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
   import PieChart from './PieChart.svelte';
 
   let data = [
-    { index: 0, name: 'Xd', value: 5 },
-    { index: 1, name: 'lmao', value: 5 },
-    { index: 2, name: 'oo', value: 90 }
+    { index: 0, name: '', value: 45 },
+    { index: 1, name: 'lmao', value: 35 },
+    { index: 2, name: 'oo', value: 25 }
   ];
 
   let parent_array: any[] = [];
@@ -27,7 +27,7 @@
 </script>
 
 <main class="flex flex-col items-center justify-center bg-[#1f1b18]">
-  <section class="flex flex-col gap-24 p-8 max-w-3xl bg-[#1f1b18] text-[#fbe7d1] z-10">
+  <section class="flex flex-col gap-48 p-8 max-w-3xl bg-[#1f1b18] text-[#fbe7d1] z-10">
     <div>
       <h1 class="text-center">Alcohol in numbers</h1>
       <p>
@@ -37,20 +37,14 @@
     </div>
     <div>
       <h3>Traffic Accident Perpetrators</h3>
-      <p><i class="twa twa-oncoming-automobile text-xl"></i> Drivers: 77%</p>
-      <p><i class="twa twa-person-walking text-xl"></i> Pedestrians: 15%</p>
-      <p><i class="twa twa-red-question-mark text-xl"></i> Others: 8%</p>
-      <div class="w-full">
-        <div class="flex flex-wrap gap-1">
-          {#each { length: 77 } as _}
-            <i class="twa twa-oncoming-automobile text-xl"></i>
-          {/each}
-          {#each { length: 15 } as _}
-            <i class="twa twa-person-walking text-xl"></i>
-          {/each}
-          {#each { length: 8 } as _}
-            <i class="twa twa-red-question-mark text-xl"></i>
-          {/each}
+      <div class="w-full pt-24 pb-44 relative flex flex-row">
+        <i class="top-28 left-52 absolute twa twa-oncoming-automobile text-6xl scale-[500%]"></i>
+        <i class="top-48 left-[432px] absolute twa twa-person-walking text-6xl"></i>
+        <i class="top-48 left-[524px] absolute twa twa-red-question-mark text-6xl scale-50"></i>
+        <div class="absolute z-10 left-[400px] top-6">
+          <p><b>77%</b> Drivers</p>
+          <p class="pl-2"><b>15%</b> Pedestrians</p>
+          <p class="pl-4"><b>8%</b> Others</p>
         </div>
       </div>
     </div>
@@ -70,11 +64,46 @@
       </div>
     </div>
     <div>
-      <h3>Youth who admitted to trying alcohol</h3>
-      <div>
-        <p>at home (45%)</p>
-        <p>at home with someone else (30%)</p>
-        <p>under a cloud (25%)</p>
+      <!-- <h3>Youth who admitted to trying alcohol</h3> -->
+      <div class="flex flex-row gap-4">
+        <div class="flex flex-col gap-4">
+          <div class="relative w-80 h-[320px] bg-sky-800 flex items-end">
+            <i class="twa twa-cloud absolute top-4 left-4 text-9xl"></i>
+            <div class="flex flex-row relative">
+              <i class="twa twa-person-standing text-9xl"></i>
+              <i
+                style="transform: scale(-1, 1);"
+                class="absolute twa twa-beer-mug text-3xl bottom-16 right-2"
+              ></i>
+            </div>
+          </div>
+          <div class="w-80 h-64 bg-indigo-800 flex items-end">
+            <div class="relative flex flex-row w-full">
+              <i class="twa twa-couch-and-lamp text-9xl"></i>
+              <i class="absolute right-16 twa twa-person-standing text-9xl"></i>
+              <i class="absolute -right-8 twa twa-man-standing text-9xl"></i>
+              <i class="absolute twa twa-clinking-beer-mugs text-4xl bottom-16 right-16"></i>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-col gap-4">
+          <div class="w-80 h-96 bg-emerald-800 flex items-end">
+            <div class="flex flex-row relative w-full">
+              <i class="twa twa-couch-and-lamp text-9xl"></i>
+              <i class="twa twa-person-standing text-9xl"></i>
+              <i
+                style="transform: scale(-1, 1);"
+                class="absolute twa twa-beer-mug text-3xl bottom-16 right-8"
+              ></i>
+            </div>
+          </div>
+          <div class="flex flex-col w-80">
+            <h4 class="text-3xl pb-3">Youth who admitted to trying alcohol</h4>
+            <p><b>45%</b> At home</p>
+            <p><b>30%</b> At home with someone else</p>
+            <p><b>25%</b> Under a cloud</p>
+          </div>
+        </div>
       </div>
     </div>
     <div>
@@ -100,7 +129,7 @@
       <div class="flex flex-col gap-2 text-xl">
         {#each [[50, 'bg-[#ea861a]', ' It was bought by an adult'], [38, 'bg-[#84cb5d]', 'The teenager was given it by an adult, e.g., at a home celebration'], [12, 'bg-[#06b6d4]', 'It was bought by a minor']] as [percentage, bg, text]}
           <div class="flex flex-row items-center gap-2">
-            <span class="w-4 h-4 {bg}"></span><b>{percentage}% - </b>
+            <span class="w-4 h-4 {bg}"></span><b>{percentage}%</b>
             {text}
           </div>
         {/each}
@@ -153,14 +182,12 @@
           ></div>
         </div>
         <div class="flex flex-col w-72 items-start justify-start text-xl">
-          <p><i class="twa twa-angry-face"></i> Strongly against <b>58%</b></p>
+          <p><i class="twa twa-angry-face"></i> <b>58%</b> Strongly against</p>
           <p>
-            <i class="twa twa-face-with-raised-eyebrow"></i> Against but with exceptions
-            <b>36%</b>
+            <i class="twa twa-face-with-raised-eyebrow"></i> <b>36%</b> Against but with exceptions
           </p>
           <p>
-            <i class="twa twa-face-with-rolling-eyes"></i> Sometimes you can turn a blind eye
-            <b>6%</b>
+            <i class="twa twa-face-with-rolling-eyes"></i> <b>6%</b> Sometimes you can turn a blind eye
           </p>
         </div>
       </div>

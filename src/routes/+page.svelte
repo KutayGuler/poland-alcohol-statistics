ca1f129f131db0f6036644c4e919cec7333fd596
KutayGuler <s7019@horyzont.eu>
2024-01-13 20:12:53 +0100

    progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
 </script>
 
 <main class="flex flex-col items-center justify-center bg-[#1f1b18]">
-  <section class="flex flex-col gap-32 p-8 max-w-3xl bg-[#1f1b18] text-[#fbe7d1] z-10">
+  <section class="flex flex-col gap-24 p-8 max-w-3xl bg-[#1f1b18] text-[#fbe7d1] z-10">
     <div>
       <h1 class="text-center">Alcohol in numbers</h1>
       <p>
@@ -36,7 +36,7 @@
       </p>
     </div>
     <div>
-      <h3>Trafic Accident Perpetrators</h3>
+      <h3>Traffic Accident Perpetrators</h3>
       <p><i class="twa twa-oncoming-automobile text-xl"></i> Drivers: 77%</p>
       <p><i class="twa twa-person-walking text-xl"></i> Pedestrians: 15%</p>
       <p><i class="twa twa-red-question-mark text-xl"></i> Others: 8%</p>
@@ -57,7 +57,7 @@
     <div>
       <h3>Polish Teenagers and Alcohol</h3>
       <p>Three-quarters of students aged 12-17 have a history of alcohol exposure</p>
-      <div class="flex flex-row gap-24 w-full h-full py-24 items-center justify-center pr-8">
+      <div class="flex flex-row gap-24 w-full h-full pt-20 pb-12 items-center justify-center pr-8">
         {#each { length: 3 } as _}
           <div class="relative w-fit">
             <i class="twa twa-melting-face text-5xl absolute -top-1"></i>
@@ -71,13 +71,44 @@
     </div>
     <div>
       <h3>Youth who admitted to trying alcohol</h3>
+      <div>
+        <p>at home (45%)</p>
+        <p>at home with someone else (30%)</p>
+        <p>under a cloud (25%)</p>
+      </div>
     </div>
     <div>
       <h3>From where did the alcohol drunk the first time come from?</h3>
+      <div class="flex flex-row gap-6 pt-10 pb-16 -rotate-3">
+        <img
+          class="w-20 h-20"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Pacman.svg/228px-Pacman.svg.png"
+        />
+        <div class="flex flex-col justify-center gap-2">
+          <div class="flex flex-row gap-2 items-center">
+            {#each { length: 17 } as _}
+              <i class="twa twa-tropical-drink text-2xl"></i>
+            {/each}
+          </div>
+          <div class="flex flex-rowh-2 w-full">
+            <div class="flex flex-row gap-2 bg-[#ea861a] h-4 w-[56%]"></div>
+            <div class="flex flex-row gap-2 bg-[#84cb5d] h-4 w-[36%]"></div>
+            <div class="flex flex-row gap-2 bg-[#06b6d4] h-4 w-[12%]"></div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 text-xl">
+        {#each [[50, 'bg-[#ea861a]', ' It was bought by an adult'], [38, 'bg-[#84cb5d]', 'The teenager was given it by an adult, e.g., at a home celebration'], [12, 'bg-[#06b6d4]', 'It was bought by a minor']] as [percentage, bg, text]}
+          <div class="flex flex-row items-center gap-2">
+            <span class="w-4 h-4 {bg}"></span><b>{percentage}% - </b>
+            {text}
+          </div>
+        {/each}
+      </div>
     </div>
     <div>
-      <h3 class="pb-4">Influence</h3>
-      <p>
+      <h3>Influence</h3>
+      <p class="pb-6">
         According to the total number of teenagers surveyed, the biggest influence on whether they
         drink alcohol is:
       </p>
@@ -118,13 +149,14 @@
             ></i>
           {/each}
           <div
-            class="border-gray-200 w-72 h-80 border-l-0 border-[32px] rounded-full -z-10 absolute top-28 left-64 rounded-t-3xl"
+            class="border-gray-200 w-72 h-80 border-l-0 border-[32px] rounded-full -z-10 absolute top-36 left-64 rounded-t-3xl"
           ></div>
         </div>
-        <div class="flex flex-col w-72 items-start justify-start">
+        <div class="flex flex-col w-72 items-start justify-start text-xl">
           <p><i class="twa twa-angry-face"></i> Strongly against <b>58%</b></p>
           <p>
-            <i class="twa twa-face-with-raised-eyebrow"></i> Against but with exceptions <b>36%</b>
+            <i class="twa twa-face-with-raised-eyebrow"></i> Against but with exceptions
+            <b>36%</b>
           </p>
           <p>
             <i class="twa twa-face-with-rolling-eyes"></i> Sometimes you can turn a blind eye
@@ -144,5 +176,9 @@
 
   h3 {
     @apply text-4xl;
+  }
+
+  p {
+    @apply text-xl;
   }
 </style>
